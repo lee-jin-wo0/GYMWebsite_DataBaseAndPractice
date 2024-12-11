94e339c5d294201dfaba814286cdee9cca9b1a7f
lee-jin-wo0 <158492273+lee-jin-wo0@users.noreply.github.com>
2024-12-11 11:18:11 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer style="background-color: #000; color: white; text-align: center; padding: 10px 0; margin-top: 20px;">
+    <p style="margin: 0;">© 2024 Hongik University Gym. 김윤호 이정혁 이진우 이재범</p>
+</footer>
